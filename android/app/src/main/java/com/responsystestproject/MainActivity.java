25ea87ffd059115143e58aab762d7163d3dc1590
VictorFlm31 <victorflm31@gmail.com>
2022-06-29 10:13:23 -0300

    adicionando config responsys nativo

--- android/app/src/main/java/com/responsystestproject/MainActivity.java
+++ android/app/src/main/java/com/responsystestproject/MainActivity.java
@@ -1,8 +1,18 @@
 package br.com.redeipiranga.debug;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import com.pushio.manager.PushIOManager;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // The following line initializes the SDK, ensures that any registration changes are reflected with Responsys,
+    // and sets up users to receive push notifications
+    PushIOManager.getInstance(getApplicationContext()).registerApp();
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
